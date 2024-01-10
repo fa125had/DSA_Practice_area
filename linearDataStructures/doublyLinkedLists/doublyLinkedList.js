90992edaf43cb7d2005ddfbe113206809315c746
far5had <far5had@outlook.com>
2024-01-11 00:36:12 +0100

    created doublyLinkedList, added addToHead, addToTail methods

--- linearDataStructures/doublyLinkedLists/doublyLinkedList.js
+++ linearDataStructures/doublyLinkedLists/doublyLinkedList.js
@@ -5,4 +5,34 @@ class DoublyLinkedList {
     this.head = null;
     this.tail = null;
   }
+
+  addToHead(data) {
+    const newHead = new Node(data);
+    const currentHead = this.head;
+    if (currentNode) {
+      currentHead.setPreviousNode(newHead);
+      newHead.setNextNode(currentHead);
+    }
+    this.head = newHead;
+
+    if (!this.tail) {
+      this.tail = newHead;
+    }
+  }
+
+  addToTail(data) {
+    const newTail = new Node(data);
+    const currentTail = this.tail;
+
+    if (currentTail) {
+      currentTail.setNextNode(newTail);
+      newTail.setPreviousNode(currentTail);
+    }
+
+    this.tail = newTail;
+
+    if (!this.head) {
+      this.head = newTail;
+    }
+  }
 }
