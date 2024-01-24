daff1784a529f7f4566028d73455c72c9b54ab9d
far5had <far5had@outlook.com>
2024-01-24 16:34:58 +0100

    added Recursion algorithm example

---
+++ algorithms/recursion.js
@@ -0,0 +1,34 @@
+/*
+Recursion is a powerful tool for solving problems that require the execution of a similar action multiple times until a certain condition(base case) is met.
+*/
+
+// Iterative factorial example
+const iterativeFactorial = (n) => {
+  let result = 1;
+  while (n > 0) {
+    result *= n;
+    n -= 1;
+  }
+  return result;
+};
+
+const number = 4;
+const iFactorial = iterativeFactorial(number);
+console.log(`Iterative Factorial of ${number}: ${iFactorial}`);
+
+// Recursive factorial example
+const recursiveFactorial = (n) => {
+  // Base Case
+  if (n === 0) return 1;
+
+  // Recursive Case
+  if (n > 0) {
+    console.log(`Execution context: ${n}`);
+
+    return recursiveFactorial(n - 1) * n;
+  }
+};
+
+const rFactorial = recursiveFactorial(number);
+console.log(`Recursive Factorial of ${number}: ${iFactorial}`);
+
