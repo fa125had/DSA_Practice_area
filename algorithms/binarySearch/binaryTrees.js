65c1497c9fa8dba06697ca28594dca2b94945234
far5had <far5had@outlook.com>
2024-02-08 22:38:05 +0100

    added binaryTree func

---
+++ algorithms/binarySearch/binaryTrees.js
@@ -0,0 +1,81 @@
+/*
+A binary tree is an efficient data structure
+for fast data storage and retrieval due to its O(log N) runtime.
+
+Each node has the following properties:
+
+- data
+
+- a depth value, where depth of 1 indicates the top level of the tree and a depth greater than 1 is 
+
+- a level somewhere lower in the tree
+
+- a left pointer that points to a left child which is itself a binary tree, 
+and must have a data lesser than the root node’s data.
+
+- a right pointer that points to a right child which is itself a binary tree, 
+and must have a data greater than the root node’s data,
+*/
+
+class BinaryTree {
+  constructor(value, depth = 1) {
+    this.value = value;
+    this.depth = depth;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(value) {
+    if (value < this.value) {
+      if (!this.left) {
+        this.left = new BinaryTree(value, this.depth + 1);
+      } else {
+        this.left.insert(value);
+      }
+    } else {
+      if (!this.right) {
+        this.right = new BinaryTree(value, this.depth + 1);
+      } else {
+        this.right.insert(value);
+      }
+    }
+  }
+
+  getNodeByValue(value) {
+    if (this.value === value) {
+      return this;
+    } else if (this.left && value < this.value) {
+      return this.left.getNodeByValue(value);
+    } else if (this.right) {
+      return this.right.getNodeByValue(value);
+    } else {
+      return null;
+    }
+  }
+
+  depthFirstTraversal() {
+    if (this.left) {
+      this.left.depthFirstTraversal();
+    }
+    console.log(`Depth=${this.depth}, Value=${this.value}`);
+    if (this.right) {
+      this.right.depthFirstTraversal();
+    }
+  }
+}
+
+
+// Test Script
+const randomize = () => Math.floor(Math.random() * 40);
+const bt = new BinaryTree(15);
+let numbers = [];
+
+for (let i = 0; i < 10; i++) {
+  numbers.push(randomize());
+  bt.insert(numbers[i]);
+}
+
+console.log(`Inserted [ ${numbers} ] to binary tree`);
+
+console.log("Depth First Traversal");
+bt.depthFirstTraversal();
