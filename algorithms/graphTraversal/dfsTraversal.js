9dc3a78431856d49a6bb94d005813e9e08f79ddd
=farshad <far5had@outlook.com>
2024-02-10 18:16:46 +0100

    added dfs grapht traversal

---
+++ algorithms/graphTraversal/dfsTraversal.js
@@ -0,0 +1,16 @@
+import { simpleGraph as testGraph } from './testGraph.js';
+
+const depthFirstTraversal = (start, visitedVertices = [start]) => {
+    console.log(start.data);
+
+    start.edges.forEach((edge) => {
+        const neighbor = edge.end;
+
+        if (!visitedVertices.includes(neighbor)) {
+            visitedVertices.push(neighbor);
+            depthFirstTraversal(neighbor, visitedVertices);
+        }
+    });
+};
+
+depthFirstTraversal(testGraph.vertices[0]);
